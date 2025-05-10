6dbcd9c228f99f306ee578b0d3e45f54e79b788e
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-11 00:36:56 +0530

    'commit'

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+import connectDB from './db';
+
+
+
+
+connectDB()
+
+
+
+
+
+/*
+import express from 'express';
+
+const app = express()
+( async () => {
+  try {
+   await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+  app.on("error", (error) => {
+    console.log("ERROR: ", error);
+    throw error;
+  })
+
+  app.listen(process.env.PORT, () => {
+    console.log('App is listing on ${process.env.PORT}');
+  })
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+}) () 
+
+*/
