5ed0875f609863300814ad049a3223f3303f5316
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-12 01:01:53 +0530

    'commit'

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -10,7 +10,7 @@ class ApiError extends Error {
     this.data = null
     this.message = message
     this.success = false
-    this.errors = this.errors
+    this.errors = errors
 
     if(statck) {
       this.statck = statck
@@ -19,3 +19,6 @@ class ApiError extends Error {
     }
   }
 }
+
+
+export {ApiError}
