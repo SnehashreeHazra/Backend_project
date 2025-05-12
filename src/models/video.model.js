cd07317351fb27469c3f9b65cf15b0f4a523d627
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-12 23:36:00 +0530

    'up'

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -3,7 +3,38 @@ import mongoose, {Schema} from 'mongoose';
 
 const videoSchema = new Schema(
   {
-
+    videoFile: {
+      type: String, //Cloudinary URL
+      required: true
+    },
+    thumbnail: {
+      type: String, //Cloudinary URL
+      required: true
+    },
+    title: {
+      type: String, 
+      required: true,
+    },
+    description: {
+      type: String, 
+      required: true,
+    },
+    duration: {
+      type: Number, 
+      required: true,
+    },
+    views: {
+      type: Number, 
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean, 
+      default: false
+    },
+    owener: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    }
   },
   {
     timestamps: true
