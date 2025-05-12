4ebc62920eecbc7ea4d617ba01b8afb3c3530b7b
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-12 23:30:55 +0530

    'commit'

---
+++ src/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose, {Schema} from 'mongoose';
+
+
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true
+    },
+    avatar: {
+      type: String, //Cloudinary URL
+      required: true
+    },
+    coverImage: {
+      type: String, //Cloudinary URL 
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+      }
+    ],
+    password: {
+      type: String,
+      required: [true, 'Pssword is required'],
+    },
+    refreshToken: {
+      type: String
+    }
+
+  },
+  {
+    timestamps: true,
+  }
+)
+
+
+
+export const User = mongoose.model("User", userSchema)
---
+++ src/models/video.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from 'mongoose';
+
+
+const videoSchema = new Schema(
+  {
+
+  },
+  {
+    timestamps: true
+  }
+
+)
+
+export const Video = mongoose.model("Video", videoSchema)
