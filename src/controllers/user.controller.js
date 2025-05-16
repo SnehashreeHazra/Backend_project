e9c3d1febd96c96b817342c24ce94279a1840ecf
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-16 23:58:24 +0530

    'commit'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -36,7 +36,9 @@ const registerUser = asyncHandler( async (req,res) => {
   const avatarLocalPath = req.files?.avatar[0]?.path;
   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-  
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required")
+  }
 })
 
 
