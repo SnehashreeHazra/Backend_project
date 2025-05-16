d1991b256bb64075244d9a836187e3a03929c07a
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-16 23:56:35 +0530

    'commit'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,6 +32,11 @@ const registerUser = asyncHandler( async (req,res) => {
   if(existedUser) {
     throw new ApiError(409, "User with email or username already existed")
   }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  
 })
 
 
