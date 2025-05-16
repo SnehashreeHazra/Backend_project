6c9bd8f2fcaba68b063c243d8ec4bf262abfe3c7
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-16 23:46:37 +0530

    'commit'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
 
 
 const registerUser = asyncHandler( async (req,res) => {
@@ -14,6 +15,13 @@ const registerUser = asyncHandler( async (req,res) => {
 
 
   const {fullName, email, username, password} = req.body
+
+
+  if(
+    [fullName, email, username, password].some((field) => field.trim() === "")
+  ){
+    throw new ApiError(400, "All fields are required")
+  }
 })
 
 
