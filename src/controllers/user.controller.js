c0c6217cc134bdbc87aec37722eeac6a0e11bc8d
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-21 11:35:40 +0530

    'commit'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -74,5 +74,36 @@ const registerUser = asyncHandler( async (req,res) => {
   )
 })
 
+const loginUser = asyncHandler( async (req, res) => {
+  //req body -> data
+  //username or email
+  //find the user
+  //password check
+  //access and refresh token
+  //send cookie
 
-export {registerUser}
+  const {email, username, password} = req.body
+
+  if(!username || !email) {
+    throw new ApiError(400, "username or email is required")
+  }
+
+  const user = await User.findOne({
+    $or: [{username}, {email}]
+  })
+
+  if (!user) {
+    throw new ApiError(404, "User does not exist")
+  }
+
+  const isPasswordValid = await user.isPasswordCorrect(password)
+
+  if(!isPasswordValid) {
+    throw new ApiError(401, "Invalid User Credentials")
+  }
+
+  
+})
+
+
+export {registerUser, loginUser}
