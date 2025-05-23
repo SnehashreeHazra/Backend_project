cd322540002363d1230d01fc615403e357d8c391
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-23 11:42:22 +0530

    'up'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -168,7 +168,7 @@ const logoutUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
     .clearCookie('accessToken', options)
-    .clearCookie('refreshToken', options);
+    .clearCookie('refreshToken', options).json(new ApiResponse(200, {}, "User logged out"))
 });
 
 export { registerUser, loginUser, logoutUser };
