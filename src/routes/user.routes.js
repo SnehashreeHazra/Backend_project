a81ab18a525db1c8434d96d962172362e6a3b35d
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-25 11:15:01 +0530

    'up'

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,7 @@ import {
   loginUser,
   logoutUser,
   registerUser,
+  refreshAccessToken,
 } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
@@ -27,5 +28,7 @@ router.route('/login').post(loginUser);
 
 //secured route
 route.route('/logout').post(verifyJWT, logoutUser);
+
+router.route('/refresh-token').post(refreshAccessToken);
 
 export default router;
