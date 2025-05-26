81fdd8e6809a08e8d727951a76471cada2b38f3e
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-26 20:30:15 +0530

    'commit'

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,6 +1,11 @@
 import mongoose, {Schema} from "mongoose";
 
-const subscriptionSchema = new Schema({})
+const subscriptionSchema = new Schema({
+  subscriber: {
+    type: Schema.Types.ObjectId,  //one who subscribing
+    ref: "User"
+  }
+})
 
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
