c9e7acccb55d4d35a4b58e85281d02ba14da5eef
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-26 20:32:17 +0530

    'commit'

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,6 +4,10 @@ const subscriptionSchema = new Schema({
   subscriber: {
     type: Schema.Types.ObjectId,  //one who subscribing
     ref: "User"
+  },
+  channel: {
+    type: Schema.Types.ObjectId,  //one to whom 'subscriber' is subscribing
+    ref: "User"
   }
 })
 
