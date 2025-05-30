d001041b65792015c406596894fc56ec3f887c21
Snehashree Hazra <snehashreehazra783@gmail.com>
2025-05-30 23:32:02 +0530

    'up'

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -257,7 +257,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     }
   }, {new:true}).select("-password")
 
-  return 
+  return res.status(200).json(new ApiResponse(200, user, "Account details updated successfully"))
 })
 
 export {
